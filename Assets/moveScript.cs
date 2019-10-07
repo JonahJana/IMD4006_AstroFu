0bf10b426979adcb40f88cbd5c9db25af368a6f0
Jonah <jonahjanakovic@cmail.carleton.ca>
2019-10-07 13:24:02 -0400

    basic movement and p1 anims

--- Assets/moveScript.cs
+++ Assets/moveScript.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class moveScript : MonoBehaviour
 {
+    public Animator animator;
+
     public float moveSpeed;
     public float rotationSpeed;
     public float jumpHeight;
@@ -14,6 +16,8 @@ public class moveScript : MonoBehaviour
     public Vector2 angularVelocity;
 
     public float tracker;
+
+    public bool isP1Punch;
 
 
     // Start is called before the first frame update
@@ -27,17 +31,72 @@ public class moveScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        tracker = Input.GetAxis("Vertical") * moveSpeed;
-
         ballAngle = ball.rotation;
-        ball.MoveRotation(ballAngle + rotationSpeed * tracker * Time.fixedDeltaTime);
+        
 
         angularVelocity.x = 1;
         angularVelocity.y = 0;
+        
+
+        //Player One
+        if (Input.GetKey(KeyCode.A))
+        {
+            float rotateUp = 1;
+            ball.MoveRotation(ballAngle + rotationSpeed * rotateUp * Time.fixedDeltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            //Player One Punch
+            isP1Punch = true;
+            animator.SetBool("IsAttacking", isP1Punch);
+        }
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            isP1Punch = false;
+            animator.SetBool("IsAttacking", isP1Punch);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            float rotateDown = -1;
+            ball.MoveRotation(ballAngle + rotationSpeed * rotateDown * Time.fixedDeltaTime);
+        }
+
+        //Player Two
+        if (Input.GetKey(KeyCode.J))
+        {
+
+        }
+        if (Input.GetKey(KeyCode.K))
+        {
+
+        }
+        if (Input.GetKey(KeyCode.L))
+        {
+
+        }
+
 
 
         ball.AddRelativeForce(angularVelocity * moveSpeed, ForceMode2D.Force);
 
-        //ball.MovePosition((Vector2)transform.position + ((Vector2)transform.forward * moveSpeed));
+        //Quit
+        if (Input.GetKey(KeyCode.Escape))
+        {
+
+        }
+
+        //Restart
+        if (Input.GetKey(KeyCode.R) || Input.GetKey(KeyCode.Space))
+        {
+
+        }
+
+        /*
+        //Start Game
+        if (Input.anyKeyDown)
+        {
+
+        }
+        */
     }
 }
---
+++ Assets/p2Move.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class p2Move : MonoBehaviour
+{
+    public Animator p2animator;
+
+    public float p2moveSpeed;
+    public float p2rotationSpeed;
+    public float jumpHeight;
+    public Rigidbody2D p2Ball;
+
+    public float p2BallAngle;
+
+    public Vector2 p2angularVelocity;
+
+    public float tracker;
+
+    public bool isp2Punch;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        p2Ball = GetComponent<Rigidbody2D>();
+
+        p2moveSpeed = 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        p2BallAngle = p2Ball.rotation;
+
+
+        p2angularVelocity.x = -1;
+        p2angularVelocity.y = 0;
+
+
+        //Player One
+        if (Input.GetKey(KeyCode.J))
+        {
+            float rotateUp = 1;
+            p2Ball.MoveRotation(p2BallAngle + p2rotationSpeed * rotateUp * Time.fixedDeltaTime);
+        }
+        if (Input.GetKey(KeyCode.K))
+        {
+            //Player One Punch
+            isp2Punch = true;
+            p2animator.SetBool("IsAttacking", isp2Punch);
+        }
+        if (Input.GetKeyUp(KeyCode.K))
+        {
+            isp2Punch = false;
+            p2animator.SetBool("IsAttacking", isp2Punch);
+        }
+        if (Input.GetKey(KeyCode.L))
+        {
+            float rotateDown = -1;
+            p2Ball.MoveRotation(p2BallAngle + p2rotationSpeed * rotateDown * Time.fixedDeltaTime);
+        }
+
+
+
+        p2Ball.AddRelativeForce(p2angularVelocity * p2moveSpeed, ForceMode2D.Force);
+
+        //Quit
+        if (Input.GetKey(KeyCode.Escape))
+        {
+
+        }
+
+        //Restart
+        if (Input.GetKey(KeyCode.R) || Input.GetKey(KeyCode.Space))
+        {
+
+        }
+
+        /*
+        //Start Game
+        if (Input.anyKeyDown)
+        {
+
+        }
+        */
+    }
+}
