31e55d84a5eb032f89333a61b06b90031d104869
Jonah <jonahjanakovic@cmail.carleton.ca>
2019-10-11 17:20:56 -0400

    idk man

---
+++ Assets/scripts/healthHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class healthHandler : MonoBehaviour
+{
+    [SerializeField] private HealthBar healthBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        float health = 1f;
+        healthBar.SetSize(health);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+    }
+}
---
+++ Assets/scripts/moveScript.cs
@@ -0,0 +1,145 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Threading;
+
+public class moveScript : MonoBehaviour
+{
+    public Animator animator;
+    public float moveSpeed;
+    public float boost;
+    public float rotationSpeed;
+
+    public Rigidbody2D ball;
+
+    public float ballAngle;
+
+    public Vector2 angularVelocity;
+    
+    public bool isP1Punch;
+    public bool isPunching;
+
+    public float punchLength = 2;
+    public float punchTimer = 4;
+
+    private float whenCanPunch;
+    private float punchDuration;
+
+    public Text p1HealthBox;
+    public int p1Health;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        p1HealthBox.text = p1Health.ToString();
+
+        ball = GetComponent<Rigidbody2D>();
+
+        isPunching = false;
+
+        whenCanPunch = Time.time;
+
+        p1Health = 10;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        p1HealthBox.text = p1Health.ToString();
+
+        if (p1Health <= 0)
+        {
+
+        }
+
+        ballAngle = ball.rotation;
+        
+        angularVelocity.x = 1 * boost;
+        angularVelocity.y = 0;
+        
+        
+
+        //rotations
+        if (Input.GetKey(KeyCode.A))
+        {
+            float rotateUp = 1 * rotationSpeed;
+            
+            ball.AddTorque(rotateUp, ForceMode2D.Force);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            float rotateDown = -1 * rotationSpeed;
+            //ball.MoveRotation(ballAngle + rotationSpeed * rotateDown * Time.fixedDeltaTime);
+
+            ball.AddTorque(rotateDown, ForceMode2D.Force);
+        }
+
+
+        if (Time.time > whenCanPunch)
+        {
+            //starting the punch
+            if(Input.GetKeyDown(KeyCode.S))
+            {
+                isPunching = true;
+                punchDuration = Time.time + punchLength;
+            }
+
+            // during the punch
+            if (Input.GetKey(KeyCode.S) && isPunching && Time.time < punchDuration)
+            {
+                //animator
+                animator.SetBool("IsAttacking", isP1Punch);
+
+                //Player One Punch
+                isP1Punch = true;
+                ball.AddRelativeForce(angularVelocity, ForceMode2D.Force);
+            }            
+
+            // stop punching
+            if (Input.GetKeyUp(KeyCode.S) && isPunching)
+            {
+                whenCanPunch = Time.time + punchTimer;
+                isPunching = false;
+                punchDuration = Time.time - 1;
+
+                isP1Punch = false;
+                animator.SetBool("IsAttacking", isP1Punch);
+            }
+        }
+        
+
+        
+
+       
+
+
+
+        //Quit
+        if (Input.GetKey(KeyCode.Escape))
+        {
+
+        }
+
+        //Restart
+        if (Input.GetKey(KeyCode.R) || Input.GetKey(KeyCode.Space))
+        {
+
+        }
+
+        /*
+        //Start Game
+        if (Input.anyKeyDown)
+        {
+
+        }
+        */
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        other.GetComponent<p2Move>().p2Health--;
+    }
+
+}
